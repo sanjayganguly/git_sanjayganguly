1d77ac4600b0e94ff5a6d3c1cec0fef0071e4409
SANJAY KUMAR GANGULY <99605031+sanjayganguly@users.noreply.github.com>
2022-02-16 22:51:57 +0530

    Add files via upload

---
+++ palindromeTrial.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package mynewtrials;
+
+import java.util.Scanner;
+
+/**
+ * @author SANJAY GANGULY
+ *
+ */
+public class palindromeTrial {
+
+		public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner scan = new Scanner(System.in);
+System.out.println("Please Enter Your First Number :: ");
+int num1=scan.nextInt();
+System.out.println("Please Enter Your Second Number :: ");
+int num2=scan.nextInt();
+for(int1=num1;i<=num2;i++)
+{
+	boolean temp=check(i);
+	if(check(i))
+		System.out.print(i+" "));
+}
+	}
+public static boolean check(int num)
+{
+	int r,sum=0;
+	int tenp=num;
+	while(num>0)
+}
+if(temp==sum)
+	return true;
+else
+	return false;
+}
+}
